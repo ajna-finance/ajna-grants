a3713054fd92ab85903dabc5446b243f72395edb
Mike <mikehathaway@makerdao.com>
2022-08-14 17:10:26 -0400

    add test outline

--- src/Token.sol
+++ src/Token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.13;
+pragma solidity 0.8.14;
 
 import { Initializable } from "@openzeppelin/openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 import { ERC20Upgradeable } from "@openzeppelin/openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
--- test/Token.t.sol
+++ test/Token.t.sol
@@ -1,12 +1,63 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.14;
 
 import "forge-std/Test.sol";
 
-contract TokenTest is Test {
-    function setUp() public {}
+import { AjnaToken } from "../src/Token.sol";
 
-    function testExample() public {
+contract TokenTest is Test {
+
+    AjnaToken internal _token;
+
+    function setUp() external {
+        _token = new AjnaToken();
+    }
+
+    function testFailCannotSendTokensToContract() external {
+        assert(false == _token.transfer(address(_token), 1));
+    }
+
+    function invariantMetadata() external {
+        assertEq(_token.name(),     "AjnaToken");
+        assertEq(_token.symbol(),   "AJNA");
+        assertEq(_token.decimals(), 18);
+
+        // TODO: check initial token supply
+    }
+
+    // TODO: implement this
+    function testTokenSupply() external {
         assertTrue(true);
     }
+
+
+    // TODO: implement this -> check can't mint additional tokens
+    function testCantMint() external {
+
+    }
+
+    // TODO: implement this
+    function testBurn() external {
+        assertTrue(true);
+    }
+
+    // TODO: implement this
+    function testPermit() external {
+        assertTrue(true);
+    }
+
+    // TODO: implement this
+    function testDelegateVotes() external {
+
+    }
+
+    // TODO: implement this
+    function testNestedDelegation() external {
+        assertTrue(true);
+    }
+
+    function testUpgrade() external {
+        assertTrue(true);
+    }
+
 }
