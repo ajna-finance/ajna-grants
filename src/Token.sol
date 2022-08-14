06b73c64ade9e0e0edbcb3dc3c4908bfad4d707e
Mike <mikehathaway@makerdao.com>
2022-08-14 15:37:30 -0400

    add initial token implementation

---
+++ src/Token.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.13;
+
+import { Initializable } from "@openzeppelin/openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import { ERC20Upgradeable } from "@openzeppelin/openzeppelin-contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { ERC20BurnableUpgradeable } from "@openzeppelin/openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import { ERC20PermitUpgradeable } from "@openzeppelin/openzeppelin-contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
+import { ERC20VotesUpgradeable } from "@openzeppelin/openzeppelin-contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
+
+contract AjnaToken is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable {
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() initializer public {
+        __ERC20_init("AjnaToken", "AJNA");
+        __ERC20Burnable_init();
+        __ERC20Permit_init("AjnaToken");
+        __ERC20Votes_init();
+
+        // 1 billion token initial supply, with 18 decimals
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from_, address to_, uint256 amount_)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._afterTokenTransfer(from_, to_, amount_);
+    }
+
+    function _mint(address to_, uint256 amount_)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._mint(to_, amount_);
+    }
+
+    function _burn(address account_, uint256 amount_)
+        internal
+        override(ERC20Upgradeable, ERC20VotesUpgradeable)
+    {
+        super._burn(account_, amount_);
+    }
+}
