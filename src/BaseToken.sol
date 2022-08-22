8eeb8bc0497e7e1c8aeea803163de0d949acfbbd
grandizzy <38490174+grandizzy@users.noreply.github.com>
2022-08-22 20:33:29 +0300

    Make base AjnaToken ERC20Votes, port voting related tests (#12)

--- src/BaseToken.sol
+++ src/BaseToken.sol
@@ -2,10 +2,12 @@
 pragma solidity 0.8.14;
 
 import { ERC20 } from "@oz/token/ERC20/ERC20.sol";
-import { ERC20Burnable } from "@oz/token/ERC20/extensions/ERC20Burnable.sol";
-import { ERC20Permit } from  "@oz/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract AjnaToken is ERC20, ERC20Burnable, ERC20Permit {
+import { ERC20Burnable } from "@oz/token/ERC20/extensions/ERC20Burnable.sol";
+import { ERC20Permit }   from  "@oz/token/ERC20/extensions/draft-ERC20Permit.sol";
+import { ERC20Votes }    from "@oz/token/ERC20/extensions/ERC20Votes.sol";
+
+contract AjnaToken is ERC20, ERC20Burnable, ERC20Permit, ERC20Votes {
     constructor(address tokenOwner_) ERC20("AjnaToken", "AJNA") ERC20Permit("AjnaToken") {
         _mint(tokenOwner_, 1_000_000_000 * 10 ** decimals());
     }
@@ -14,11 +16,23 @@ contract AjnaToken is ERC20, ERC20Burnable, ERC20Permit {
     /*** Overrides ***/
     /*****************/
 
+    function _afterTokenTransfer(address from_, address to_, uint256 amount_) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from_, to_, amount_);
+    }
+
     /**
      *  @notice Ensure tokens cannot be transferred to token contract
      */
     function _beforeTokenTransfer(address, address to_, uint256) internal view override {
         require(to_ != address(this), "Cannot transfer tokens to the contract itself");
+    }
+
+    function _burn(address account_, uint256 amount_) internal override(ERC20, ERC20Votes) {
+        super._burn(account_, amount_);
+    }
+
+    function _mint(address to_, uint256 amount_) internal override(ERC20, ERC20Votes) {
+        super._mint(to_, amount_);
     }
 
     /**************************/
--- test/BaseToken.t.sol
+++ test/BaseToken.t.sol
@@ -17,6 +17,8 @@ contract AjnaTokenTest is Test {
     uint256 _initialAjnaTokenSupply   = 1_000_000_000 * 1e18;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
+    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+    event DelegateVotesChanged(address indexed delegate, uint256 previousBalance, uint256 newBalance);
 
     function setUp() external {
         vm.startPrank(_tokenDeployer);
@@ -50,6 +52,10 @@ contract AjnaTokenTest is Test {
         assertEq(_token.balanceOf(_tokenHolder), _initialAjnaTokenSupply);
     }
 
+    /**********************/
+    /*** Burnable tests ***/
+    /**********************/
+
     function testBurn() external {
         assertEq(_token.totalSupply(), _initialAjnaTokenSupply);
         _token.burn(50_000_000 * 1e18);
@@ -67,6 +73,10 @@ contract AjnaTokenTest is Test {
         assertEq(_token.balanceOf(_tokenHolder), _initialAjnaTokenSupply - amount_);
         assertEq(_token.balanceOf(address(5555)),  amount_);
     }
+
+    /*********************/
+    /*** Permit tests ***/
+    /*********************/
 
     function testTransferWithPermit(uint256 amount_) external {
         vm.assume(amount_ > 0);
@@ -129,5 +139,149 @@ contract AjnaTokenTest is Test {
         assertEq(_token.balanceOf(spender),  0);
         assertEq(_token.balanceOf(newOwner), amount_ * 2);
         assertEq(_token.allowance(owner, spender), 0);
+    }
+
+    /*********************/
+    /*** Voting tests ***/
+    /*********************/
+
+    function testDelegateVotes() external {
+        address delegator = address(2222);
+        address delegate1 = address(3333);
+        address delegate2 = address(4444);
+
+        // set delegator balance to 1_000 AJNA tokens
+        deal(address(_token), delegator, 1_000 * 1e18);
+
+        // check voting power
+        assertEq(_token.getVotes(delegator), 0);
+        assertEq(_token.getVotes(delegate1), 0);
+        assertEq(_token.getVotes(delegate2), 0);
+
+        vm.roll(11111);
+        changePrank(delegator);
+        vm.expectEmit(true, true, false, true);
+        emit DelegateChanged(delegator, address(0), delegate1);
+        vm.expectEmit(true, true, false, true);
+        emit DelegateVotesChanged(delegate1, 0, 1_000 * 1e18);
+        _token.delegate(delegate1);
+
+        // check accounts balances
+        assertEq(_token.balanceOf(delegator), 1_000 * 1e18);
+        assertEq(_token.balanceOf(delegate1), 0);
+        assertEq(_token.balanceOf(delegate2), 0);
+        // check voting power
+        assertEq(_token.getVotes(delegator), 0);
+        assertEq(_token.getVotes(delegate1), 1_000 * 1e18);
+        assertEq(_token.getVotes(delegate2), 0);
+
+        assertEq(_token.delegates(delegator), delegate1);
+
+        vm.roll(11112);
+
+        changePrank(delegator);
+        vm.expectEmit(true, true, false, true);
+        emit DelegateChanged(delegator, delegate1, delegate2);
+        vm.expectEmit(true, true, false, true);
+        emit DelegateVotesChanged(delegate1, 1_000 * 1e18, 0);
+        vm.expectEmit(true, true, false, true);
+        emit DelegateVotesChanged(delegate2, 0, 1_000 * 1e18);
+        _token.delegate(delegate2);
+        vm.roll(11113);
+
+        // check accounts balances
+        assertEq(_token.balanceOf(delegator), 1_000 * 1e18);
+        assertEq(_token.balanceOf(delegate1), 0);
+        assertEq(_token.balanceOf(delegate2), 0);
+        // check voting power
+        assertEq(_token.getVotes(delegator), 0);
+        assertEq(_token.getVotes(delegate1), 0);
+        assertEq(_token.getVotes(delegate2), 1_000 * 1e18);
+
+        assertEq(_token.delegates(delegator), delegate2);
+        // check voting power at block 11111 and 11112
+        assertEq(_token.getPastVotes(delegate1, 11111), 1_000 * 1e18);
+        assertEq(_token.getPastVotes(delegate2, 11111), 0);
+        assertEq(_token.getPastVotes(delegate1, 11112), 0);
+        assertEq(_token.getPastVotes(delegate2, 11112), 1_000 * 1e18);
+    }
+
+    function testCalculateVotingPower() external {
+        address delegator1 = address(1111);
+        address delegator2 = address(2222);
+        address delegate1  = address(3333);
+        address delegate2  = address(4444);
+
+        // set delegators and delegates AJNA balances
+        deal(address(_token), delegator1, 1_000 * 1e18);
+        deal(address(_token), delegator2, 2_000 * 1e18);
+        deal(address(_token), delegate1,  3_000 * 1e18);
+        deal(address(_token), delegate2,  4_000 * 1e18);
+
+        // initial voting power is 0
+        assertEq(_token.getVotes(delegator1), 0);
+        assertEq(_token.getVotes(delegator2), 0);
+        assertEq(_token.getVotes(delegate1),  0);
+        assertEq(_token.getVotes(delegate2),  0);
+
+        // delegates delegate to themselves
+        changePrank(delegate1);
+        _token.delegate(delegate1);
+        changePrank(delegate2);
+        _token.delegate(delegate2);
+
+        // check votes
+        assertEq(_token.getVotes(delegator1), 0);
+        assertEq(_token.getVotes(delegator2), 0);
+        assertEq(_token.getVotes(delegate1),  3_000 * 1e18);
+        assertEq(_token.getVotes(delegate2),  4_000 * 1e18);
+
+        // delegators delegate to delegates
+        changePrank(delegator1);
+        _token.delegate(delegate1);
+        changePrank(delegator2);
+        _token.delegate(delegate2);
+
+        // check votes
+        assertEq(_token.getVotes(delegator1), 0);
+        assertEq(_token.getVotes(delegator2), 0);
+        assertEq(_token.getVotes(delegate1),  4_000 * 1e18);
+        assertEq(_token.getVotes(delegate2),  6_000 * 1e18);
+
+        assertEq(_token.delegates(delegator1), delegate1);
+        assertEq(_token.delegates(delegator2), delegate2);
+        assertEq(_token.delegates(delegate1),  delegate1);
+        assertEq(_token.delegates(delegate2),  delegate2);
+    }
+
+    function testNestedDelegation() external {
+        assertEq(_token.getVotes(_tokenHolder),  0);
+        assertEq(_token.getVotes(address(3333)), 0);
+        assertEq(_token.getVotes(address(4444)), 0);
+
+        // tokens owner delegates votes to 3333
+        vm.roll(11112);
+        _token.delegate(address(3333));
+
+        assertEq(_token.getVotes(_tokenHolder),  0);
+        assertEq(_token.getVotes(address(3333)), 1_000_000_000 * 1e18);
+        assertEq(_token.getVotes(address(4444)), 0);
+
+        // 3333 cannot delegate votes to 4444
+        vm.roll(11112);
+        changePrank(address(3333));
+        _token.delegate(address(4444));
+
+        assertEq(_token.getVotes(_tokenHolder),  0);
+        assertEq(_token.getVotes(address(3333)), 1_000_000_000 * 1e18);
+        assertEq(_token.getVotes(address(4444)), 0);
+
+        // tokens owner delegates votes to 4444
+        changePrank(_tokenHolder);
+        _token.delegate(address(4444));
+
+        assertEq(_token.getVotes(_tokenHolder),  0);
+        assertEq(_token.getVotes(address(3333)), 0);
+        assertEq(_token.getVotes(address(4444)), 1_000_000_000 * 1e18);
     }
 }
