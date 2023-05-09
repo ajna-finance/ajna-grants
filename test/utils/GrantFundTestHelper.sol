02fa12958b0bc1e2486cc22faf7ebb64d2e94719
grandizzy <grandizzy.the.egg@gmail.com>
2023-05-09 07:57:54 +0300

    Quantstamp: Code Documentation & Best Practices

--- test/utils/GrantFundTestHelper.sol
+++ test/utils/GrantFundTestHelper.sol
@@ -751,6 +751,11 @@ abstract contract GrantFundTestHelper is Test {
         _fundingVoteNoLog(grantFund_, voter_, proposalId_, votesAllocated_);
     }
 
+    function assertInsufficientRemainingVotingPowerRevert(GrantFund grantFund_, address voter_, uint256 proposalId_, int256 votesAllocated_) internal {
+        vm.expectRevert(IStandardFunding.InsufficientRemainingVotingPower.selector);
+        _fundingVoteNoLog(grantFund_, voter_, proposalId_, votesAllocated_);
+    }
+
     function assertFundingVoteInvalidVoteRevert(GrantFund grantFund_, address voter_, uint256 proposalId_, int256 votesAllocated_) internal {
         vm.expectRevert(IStandardFunding.InvalidVote.selector);
         _fundingVoteNoLog(grantFund_, voter_, proposalId_, votesAllocated_);
