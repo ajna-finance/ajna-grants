ab13cb06db07b1462b62e0fc369cd19b29c4b284
Mike <mikehathaway@makerdao.com>
2023-06-21 13:46:54 -0400

    update makefile

--- test/invariants/test-invariant.sh
+++ test/invariants/test-invariant.sh
@@ -5,10 +5,10 @@ echo "Exporting environment variables"
 
 # export environment variables retrieved from user input
 export SCENARIO=${1:-${SCENARIO}}
-export NUM_ACTORS=${2:-${NUM_ACTORS}}
-export NUM_PROPOSALS=${3:-${NUM_PROPOSALS}}
-export PER_ADDRESS_TOKEN_REQ_CAP=${4:-${PER_ADDRESS_TOKEN_REQ_CAP}}
-export LOGS_VERBOSITY=${5:-${LOGS_VERBOSITY}}
+export LOGS_VERBOSITY=${2:-${LOGS_VERBOSITY}}
+export NUM_ACTORS=${3:-${NUM_ACTORS}}
+export NUM_PROPOSALS=${4:-${NUM_PROPOSALS}}
+export PER_ADDRESS_TOKEN_REQ_CAP=${5:-${PER_ADDRESS_TOKEN_REQ_CAP}}
 
 echo "Running invariant test"
 
